cb8bd4bf3095bc67a2e9d1a977114d42fcfea720
Makenzie Cope <mcope3208@gmail.com>
2024-03-29 21:33:24 -0500

    Created the sign in button and a message for users if they do not input their account correctly

--- z2_SignIn.py
+++ z2_SignIn.py
@@ -20,4 +20,13 @@ password_label.grid(row=1, column=0, sticky="w")
 password_entry = ttk.Entry(main_frame, show="*")  # Show asterisks for password
 password_entry.grid(row=1, column=1, sticky="w")
 
+login_button = ttk.Button(main_frame, text="Sign In", command=login)
+login_button.grid(row=2, column=0, columnspan=2, pady=(10, 0))
+
+message_label = ttk.Label(main_frame, text="", foreground="red")
+message_label.grid(row=3, column=0, columnspan=2)
+
+signin_button = ttk.Button(root, text='Need an Account? Sign up here.', command=open_signup_window)
+signin_button.grid(row=5, column=0, columnspan=2, padx=10, pady=5)
+
 root.mainloop()
