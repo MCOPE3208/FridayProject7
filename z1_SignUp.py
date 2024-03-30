575f671e21c3128eaaac153cab92eed89718a8c1
Makenzie Cope <mcope3208@gmail.com>
2024-03-29 21:21:20 -0500

    Create confirm password entry box

--- z1_SignUp.py
+++ z1_SignUp.py
@@ -13,4 +13,9 @@ password_label.grid(row=1, column=0, padx=10, pady=5, sticky=tk.E)
 password_entry = ttk.Entry(root, show='*')
 password_entry.grid(row=1, column=1, padx=10, pady=5)
 
+confirm_password_label = ttk.Label(root, text='Confirm Password:')
+confirm_password_label.grid(row=2, column=0, padx=10, pady=5, sticky=tk.E)
+confirm_password_entry = ttk.Entry(root, show='*')
+confirm_password_entry.grid(row=2, column=1, padx=10, pady=5)
+
 root.mainloop()
